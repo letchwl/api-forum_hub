1442b5863704ca6833c92be79b62db320ef3dd90
letch <raullalvespe1227@outlook.com.br>
2025-05-15 21:46:06 -0300

    Token JWT

---
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -0,0 +1,34 @@
+package br.com.forum_hub.controller;
+
+import br.com.forum_hub.domain.autenticacao.DadosLogin;
+import br.com.forum_hub.domain.autenticacao.TokenService;
+import br.com.forum_hub.domain.usuario.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutenticacaoController {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public AutenticacaoController(AuthenticationManager authenticationManager, AuthenticationManager authenticationManager1, TokenService tokenService) {
+        this.authenticationManager = authenticationManager1;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> efetuarLogin(@RequestBody @Valid DadosLogin dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = authenticationManager.authenticate(authenticationToken);
+
+        String tokenAcesso = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(tokenAcesso);
+    }
+}
--- src/main/java/br/com/forum_hub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/forum_hub/infra/security/SecurityConfigurations.java
@@ -1,4 +1,37 @@
 package br.com.forum_hub.infra.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .build();
+        
+    }
+
+    @Bean
+    public PasswordEncoder encryptador() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
