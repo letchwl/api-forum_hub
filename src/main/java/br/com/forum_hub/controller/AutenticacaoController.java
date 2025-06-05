cfdcfaee7a54bb70ccd61ded2c5016537783e473
letchwl <raullalvespe1227@outlook.com.br>
2025-06-05 18:52:40 -0300

    Refresh Token

--- src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -1,9 +1,11 @@
 package br.com.forum_hub.controller;
 
 import br.com.forum_hub.domain.autenticacao.DadosLogin;
+import br.com.forum_hub.domain.autenticacao.DadosRefreshToken;
 import br.com.forum_hub.domain.autenticacao.DadosToken;
 import br.com.forum_hub.domain.autenticacao.TokenService;
 import br.com.forum_hub.domain.usuario.Usuario;
+import br.com.forum_hub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,10 +19,12 @@ public class AutenticacaoController {
 
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+    private final UsuarioRepository usuarioRepository;
 
-    public AutenticacaoController(AuthenticationManager authenticationManager, AuthenticationManager authenticationManager1, TokenService tokenService) {
+    public AutenticacaoController(AuthenticationManager authenticationManager, AuthenticationManager authenticationManager1, TokenService tokenService, UsuarioRepository usuarioRepository) {
         this.authenticationManager = authenticationManager1;
         this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @PostMapping("/login")
@@ -33,4 +37,17 @@ public class AutenticacaoController {
 
         return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken));
     }
+
+    @PostMapping("/atualizar-token")
+    public ResponseEntity<DadosToken> atualizarToken(@Valid @RequestBody DadosRefreshToken dados) {
+        var refreshToken = dados.refreshToken();
+        Long idUsuario = Long.valueOf(tokenService.verificarToken(refreshToken));
+        var usuario = usuarioRepository.findById(idUsuario).orElseThrow();
+
+        String tokenAcesso = tokenService.gerarToken(usuario);
+        String tokenAtualizacao = tokenService.gerarRefreshToken(usuario);
+
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, tokenAtualizacao));
+    }
+
 }
